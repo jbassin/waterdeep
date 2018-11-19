13e07e12493566bd2113fe9f76ffea850a3d6941
jbassin <jbassin@bu.edu>
2018-11-19 12:48:13 -0500

    Added relationship graph renderer

--- src/components/factions/relationships.vue
+++ src/components/factions/relationships.vue
@@ -1,19 +1,79 @@
 <template>
-  <div class="container animated fadeIn">
-    <div class="notification is-primary">
-      <p class="title is-4">
-        Relationships
-      </p>
+  <keep-alive>
+    <div class="container animated fadeIn">
+      <div id="relationship-graph-header"
+           class="notification is-primary">
+        <p class="title is-4">
+          Relationships
+        </p>
+        <cytoscape :config="config"
+                   style="height: 650px"></cytoscape>
+      </div>
     </div>
-  </div>
+  </keep-alive>
 </template>
 
 <script>
+/* eslint-disable no-unused-vars,no-var */
+
 export default {
   name: 'ccRelationships',
+  data() {
+    return {
+      config: {
+        elements: [
+          { // node a
+            data: { id: 'a' },
+          }, { // node b
+            data: { id: 'b' },
+          }, { // edge ab
+            data: {
+              id: 'ab',
+              source: 'a',
+              target: 'b',
+            },
+          },
+        ],
+        style: [
+          {
+            selector: 'node',
+            style: {
+              'background-color': '#fff',
+              label: 'data(id)',
+            },
+          }, {
+            selector: 'edge',
+            style: {
+              width: 3,
+              'line-color': '#fff',
+              'target-arrow-color': '#ccc',
+              'target-arrow-shape': 'triangle',
+            },
+          },
+        ],
+        layout: {
+          name: 'grid',
+          rows: 1,
+        },
+      },
+    };
+  },
+  methods: {
+    resizeCanvas() {
+      // const canvas = document.getElementById('relationship-graph');
+      // const parent = document.getElementById('relationship-graph-header');
+      // canvas.width = parent.offsetWidth - canvas.offsetLeft * 2;
+      // canvas.height = parent.offsetHeight - canvas.offsetTop - 25;
+    },
+  },
+  mounted() {
+    this.resizeCanvas();
+  },
 };
 </script>
 
 <style scoped>
-
+  .notification {
+    height: 750px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 import Vue from 'vue';
+import VueCytoscape from 'vue-cytoscape';
+import 'vue-cytoscape/dist/vue-cytoscape.css';
 import { ObserveVisibility } from 'vue-observe-visibility';
 import App from './App.vue';
 import router from './router';
@@ -7,6 +9,7 @@ import store from './store';
 
 Vue.directive('observe-visibility', ObserveVisibility);
 Object.defineProperty(Vue.prototype, '$_', { value: _ });
+Vue.use(VueCytoscape);
 Vue.config.productionTip = false;
 
 new Vue({
