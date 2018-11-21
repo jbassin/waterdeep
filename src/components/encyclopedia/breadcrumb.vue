537f1cc30a3f4352de1eec4714acbff3e34a820e
jbassin <jbassin@bu.edu>
2018-11-20 19:40:25 -0500

    Moved breadcrumbs to separate component

---
+++ src/components/encyclopedia/breadcrumb.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="breadcrumb has-arrow-separator is-right">
+    <ul>
+      <li v-for="(path, index) in currentRoute"
+          :key="index"
+          :class="{ 'is-active': path.isActive }">
+        <a @click="go(path.path)">
+          {{ path.header }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'ccBreadcrumb',
+  props: {
+    currentRoute: { required: true },
+  },
+  methods: {
+    go(path) {
+      this.$router.replace({
+        path: `/encyclopedia${path}`,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/components/encyclopedia/encyclopedia.vue
+++ src/components/encyclopedia/encyclopedia.vue
@@ -1,22 +1,51 @@
 <template>
   <div class="container animated fadeIn">
-    <div class="notification is-primary">
-      <p class="title is-4">
-        Encyclopedia
-      </p>
-      {{ currentRoute }}
-    </div>
+    <keep-alive>
+      <div class="notification is-primary">
+        <nav class="level">
+          <div class="level-left">
+            <div class="level-item">
+              <p class="title is-4">
+                Encyclopedia
+              </p>
+            </div>
+          </div>
+          <div class="level-right">
+            <div class="level-item">
+              <ccBreadcrumb :current-route="currentRoute"></ccBreadcrumb>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 /* eslint-disable no-underscore-dangle */
+import ccBreadcrumb from './breadcrumb.vue';
 
 export default {
   name: 'ccEncyclopedia',
+  components: {
+    ccBreadcrumb,
+  },
   computed: {
     currentRoute() {
-      return this.$route.params['0'] ? this.$route.params['0'].split('/') : ['index'];
+      const routeArray = [];
+      this.$_.each(this.$route.params['0'] ? this.$route.params['0'].split('/') : ['index'], (item, index) => {
+        routeArray.push({
+          header: item,
+          path: routeArray[index - 1] ? `${routeArray[index - 1].path}/${item}` : `/${item}`,
+          isActive: false,
+        });
+      });
+      routeArray.slice(-1)[0].isActive = true;
+      return routeArray;
+    },
+    currentEntry() {
+      return this.currentRoute.slice(-1)[0].header;
     },
   },
 };
